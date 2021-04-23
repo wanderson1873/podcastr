187030c18fb72b7d0105c01b736e57adcaf02e39
Wanderson Oliveira <wanderson1873@live.com>
2021-04-23 02:03:14 +0100

    add

--- podcastrnext/src/pages/_app.tsx
+++ podcastrnext/src/pages/_app.tsx
@@ -1,19 +1,41 @@
-import '../style/global.scss'
+import React from 'react'
 
+import '../style/global.scss'
 import { Header } from '../components/Header'
 import { Player } from '../components/Player'
+import { PlayerContext } from '../contexts/PlayerContext'
 
 import styles from '../style/app.module.scss'
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = React.useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = React.useState(0);
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
