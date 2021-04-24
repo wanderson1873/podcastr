73c7e1fbe0b46a65cf867e2974cb4e19f30b67f5
Wanderson Oliveira <wanderson1873@live.com>
2021-04-24 10:58:17 +0100

    episodeList

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -28,7 +28,9 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
-  const { play } = useContext(PlayerContext)
+  const { playList } = useContext(PlayerContext);
+
+  const episodeList = [...latestEpisodes, ...allEpisodes];
 
   return (
     <div className={styles.homepage}>
@@ -36,7 +38,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
         <h2>Últimos lançamentos</h2>
 
         <ul>
-          {latestEpisodes.map(episode => {
+          {latestEpisodes.map((episode, index) => {
             return (
               <li key={episode.id}>
                   <Image 
@@ -57,7 +59,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                     <span>{episode.durationAsString}</span>
                   </div>
 
-                  <button type="button" onClick={() => play(episode)}>
+                  <button type="button" onClick={() => playList(episodeList, index)}>
                     <img src="/play-green.svg" alt="Tocar episodio"/>
                   </button>
               </li>
@@ -79,7 +81,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
               <th></th>
             </thead>
             <tbody>
-              {allEpisodes.map(episode => {
+              {allEpisodes.map((episode, index) => {
                 return (
                   <tr key={episode.id}> 
                     <td style={{width: 72}}>
@@ -100,7 +102,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                     <td style={{width: 100}}>{episode.publishedAt}</td>
                     <td>{episode.durationAsString}</td>
                     <td>
-                      <button>
+                      <button type="button" onClick={() => playList(episodeList, index + latestEpisodes.length)}>
                         <img src="/play-green.svg" alt="Tocar episódio"/>
                       </button>
                     </td>
