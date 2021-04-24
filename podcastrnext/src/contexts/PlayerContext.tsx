81f0c6fa5c68a02709c38c54d3fb1554e104fbd9
Wanderson Oliveira <wanderson1873@live.com>
2021-04-24 10:57:49 +0100

    Desacoplando

--- podcastrnext/src/contexts/PlayerContext.tsx
+++ podcastrnext/src/contexts/PlayerContext.tsx
@@ -1,3 +1,4 @@
+import React, { ReactNode } from 'react'
 import { createContext } from 'react'
 
 type Episode = {
@@ -12,9 +13,65 @@ type PlayerContextData = {
    episodeList: Episode[];
    currentEpisodeIndex: number;
    isPlaying: boolean;
+   playList: (list: Episode[], index: number) => void; 
    play: (episode: Episode) => void;
    setPlayingState: (state: boolean) => void;
    togglePlay: () => void;
 }
 
 export const PlayerContext = createContext({} as PlayerContextData)
+
+type PlayerContextDataProviderProps = {
+   children: ReactNode;
+}
+
+export function PlayerContextProvider({ children }: PlayerContextDataProviderProps) {
+   const [episodeList, setEpisodeList] = React.useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = React.useState(0);
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function playList(list: Episode[], index: number) {
+     setEpisodeList(list);
+     setCurrentEpisodeIndex(index);
+     setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  function playNext() {
+     const nextEpisodeIndex = currentEpisodeIndex + 1
+
+     if(nextEpisodeIndex < episodeList.length) 
+     setCurrentEpisodeIndex(currentEpisodeIndex + 1);
+  }
+
+  
+
+  return (
+    <PlayerContext.Provider 
+      value={{ 
+         episodeList, 
+         currentEpisodeIndex, 
+         play, 
+         playList,
+         isPlaying, 
+         togglePlay, 
+         setPlayingState,
+      }}
+   >
+       {children}
+    </PlayerContext.Provider>
+  )
+}
